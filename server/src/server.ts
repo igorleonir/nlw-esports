fc6a6ae4bf730a46c5bc3cd6ad83816e563434d8
IgorLeonir <igorleonir@gmail.com>
2022-09-17 18:29:01 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,91 @@
 import express from 'express'
+import cors from 'cors'
+
+import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
-  return response.json('a')
+app.use(express.json())
+app.use(cors())
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
+app.post('/games/:id/ad', async (request, response) => {
+  const gameId = request.params.id
+  const body = request.body
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hoursStart),
+      hourEnd: convertHourStringToMinutes(body.hoursEnd),
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+})
+
+
+
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  })
+})
+
+app.get('/games/:id/ad', async (request, response) => {
+  const gameId = request.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hoursEnd: true,
+    },
+    where: {
+      gameId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  return response
+    .json(ads.map((ad) => ({ ...ad, weekDays: ad.weekDays.split(',') })))
+    .status(200)
+})
+
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id
+
+  const ad = await prisma.id.findUniqueOrThrow({
+    select: {
+      discord: true
+    },
+    where: {
+      id: adId
+    }
+  })
+
+  return response.json({
+    discord: ad.discord
+  })
 })
 
 
